1b5c20bd565c652ee501644e3a3cfb06f4a5797c
Vega-Gallardo-Brandon-Alexander <vega2bran@gmail.com>
2025-12-01 00:14:00 -0600

    Change message

--- corrector.cpp
+++ corrector.cpp
@@ -347,7 +347,7 @@ int main() {
                          << " [" << weights[i] << "]" << endl;
                 }
             } else {
-                cout << "No hay sugerencias." << endl;
+                cout << "Sin sugerencias." << endl;
             }
         }
     }
